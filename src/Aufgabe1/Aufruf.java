0bbdd6b982c9a967d7633adcbe9aa4ca4db3969f
nbochejny <nbochejny@gmail.com>
2018-12-18 17:42:14 +0100

    Aufgabe1

---
+++ src/Aufgabe1/Aufruf.java
@@ -0,0 +1,10 @@
+package Aufgabe1;
+
+public class Aufruf {
+
+    public static void main (String[]args){
+        Mensch Nr1 = new Mensch("Nicolas",20,"männlich");
+        System.out.println(Nr1.printAll());
+    }
+
+}
---
+++ src/Aufgabe1/Saeugetier.java
@@ -0,0 +1,41 @@
+package Aufgabe1;
+
+public abstract class Saeugetier {
+    private String name;
+    private int alter;
+    private String geschlecht;
+
+    public Saeugetier(String name, int alter, String geschlecht){
+        this.alter=alter;
+        this.name=name;
+        this.geschlecht=geschlecht;
+    }
+
+
+    public abstract String printAll();
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAlter() {
+        return alter;
+    }
+
+    public void setAlter(int alter) {
+        this.alter = alter;
+    }
+
+    public String getGeschlecht() {
+        return geschlecht;
+    }
+
+    public void setGeschlecht(String geschlecht) {
+        this.geschlecht = geschlecht;
+    }
+}
